87722f244fb4b2389a48fa725fcbaec195e081fc
Mo <test@gmail.com>
2020-09-23 16:30:43 +1000

    str 65

---
+++ magicians.py
@@ -0,0 +1,14 @@
+print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician)
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician.title() + ", that was a great trick!")
+magicians = ['alice', 'david', 'carolina']
+for magician in magicians:
+    print(magician.title() + ", that was a great trick!")
+    print("I can't wait to see your next trick, " + magician.title() + ".\n")
+print("The END")
+
+

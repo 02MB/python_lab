58edbe8c917f62d33758ba5af93541b46988560a
Mo <test@gmail.com>
2020-09-24 17:57:53 +1000

    str 71

---
+++ squares.py
@@ -0,0 +1,21 @@
+squres = []
+for values in range(1,11):
+    squre = values**2
+    squres.append(squre)
+    print(squres)
+print(squres)
+
+squres = []
+for values in range(1,11):
+    squres.append(values**2)
+    print(squres)
+print(squres)
+
+# digits = [1,2,3,4,5,6,7,8,9,0]
+number_list = []
+for numbers_list in range(0,10):
+    number_list.append(numbers_list)
+print(number_list)
+print("min: " + str(min(number_list)))
+print("max: " + str(max(number_list)))
+print("sum: " + str(sum(number_list)))
--- test.py
+++ test.py
@@ -1,2 +1,6 @@
-test = "test"
-print(test)
+number_list = range(1,6)
+for numbers in number_list:
+    # print("This is number")
+    # print(numbers)
+    print("This is number: " + str(numbers))
+
